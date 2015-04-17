d474e44fbf5a04c418fbd5c25997d3f5cb263f7b
suyash08 <suyash08@gmail.com>
2015-04-17 16:25:23 +0530

    This is first user commit when this servlet is created with overloaded methods.

---
+++ HelloWorldServlet/src/com/smb/servlet/MyFirstServletWithGitHub.java
@@ -0,0 +1,45 @@
+package com.smb.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServletWithGitHub
+ */
+public class MyFirstServletWithGitHub extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFirstServletWithGitHub() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("This is a init method for initialization");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("This is a simple get method");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("This is a simple post method");
+	}
+
+}
